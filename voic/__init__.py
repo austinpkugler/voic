4b08c8a6195491fe8a1ac29f02fce61664f13f5f
austinpkugler <austinpkugler@gmail.com>
2022-04-09 13:27:48 -0700

    Add template for database

--- voic/__init__.py
+++ voic/__init__.py
@@ -1,5 +1,11 @@
 import flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = flask.Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///voic.db'
+
+db = SQLAlchemy(app)
+
 from voic import routes
