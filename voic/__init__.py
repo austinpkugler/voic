e29b0ee4d96d332777c3b6b9cd248748f4abeb1a
Hayden Carroll <haydenccarroll@gmail.com>
2022-04-09 14:51:06 -0700

    added routes + templates for website, also added line in init

--- voic/__init__.py
+++ voic/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///voic.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///voic.db'
 
 db = SQLAlchemy(app)
 
--- voic/routes.py
+++ voic/routes.py
@@ -7,3 +7,31 @@ from voic import app
 @app.route('/home')
 def home():
     return flask.render_template('home.html')
+
+@app.route('/my-documents')
+def my_documents():
+    return flask.render_template('my-documents.html')
+
+@app.route('/new-document')
+def new_document():
+    return flask.render_template('forms/new-document.html')
+
+@app.route('/edit-document')
+def edit_document():
+    return flask.render_template('forms/edit-document.html')
+
+@app.route('/sign-in')
+def sign_in():
+    return flask.render_template('forms/sign-in.html')
+
+@app.route('/sign-up')
+def sign_up():
+    return flask.render_template('forms/sign-up.html')
+
+@app.route('/account')
+def account():
+    return flask.render_template('forms/account.html')
+
+@app.route('/sign-out')
+def sign_out():
+    return flask.render_template('sign-out.html')
