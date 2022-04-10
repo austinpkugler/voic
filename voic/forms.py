f2875892ba0aa24fbb514767439959d8e132e3d9
austinpkugler <austinpkugler@gmail.com>
2022-04-09 22:28:52 -0700

    First working account management

--- voic/forms.py
+++ voic/forms.py
@@ -1,51 +1,65 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+import flask_login
+import flask_wtf
+from flask_wtf.file import (
+    FileField,
+    FileAllowed
+)
+from wtforms import (
+    BooleanField,
+    PasswordField,
+    StringField,
+    SubmitField
+)
+from wtforms.validators import (
+    DataRequired,
+    Email,
+    EqualTo,
+    Length,
+    ValidationError
+)
 
-import voic.models
+from voic import models
 
 
-class RegistrationForm(FlaskForm):
+class SignUpForm(flask_wtf.FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        user = voic.models.User.query.filter_by(username=username.data).first()
+        user = models.User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('Username already exists.')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
+        user = models.User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('Email already exists.')
 
 
-class LoginForm(FlaskForm):
+class SignInForm(flask_wtf.FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 
-class UpdateAccountForm(FlaskForm):
+class UpdateAccountForm(flask_wtf.FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first()
+        if username.data != flask_login.current_user.username:
+            user = models.User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError('Username already exists.')
 
     def validate_email(self, email):
-        if email.data != current_user.email:
-            user = User.query.filter_by(email=email.data).first()
+        if email.data != flask_login.current_user.email:
+            user = models.User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('Email already exists.')
