236ad9d0f11354f24a64ccc43621837d4bfc4725
austinpkugler <austinpkugler@gmail.com>
2022-04-14 10:22:49 -0700

    Add mobile specific rendering

--- voic/__init__.py
+++ voic/__init__.py
@@ -3,6 +3,7 @@ import flask_bcrypt
 import flask_ckeditor
 import flask_login
 import flask_sqlalchemy
+import flask_mobility
 
 import logging
 import os
@@ -17,6 +18,7 @@ app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///voic.db'
+mobility = flask_mobility.Mobility(app)
 db = flask_sqlalchemy.SQLAlchemy(app)
 bcrypt = flask_bcrypt.Bcrypt(app)
 login_manager = flask_login.LoginManager(app)
